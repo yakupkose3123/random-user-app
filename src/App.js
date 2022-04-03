3f586bd56435518be567898659eb56573b87b2b1
yakupkose3123 <yakupkose3123@gmail.com>
2022-04-04 02:28:24 +0300

    added linner gradient for css

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Users from './components/users/Users';
+
+function App() {
+  return (
+    <div className="App">
+     <Users/>
+    </div>
+  );
+}
+
+export default App;
